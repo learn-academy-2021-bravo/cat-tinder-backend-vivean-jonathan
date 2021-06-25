1c4a7b5f0edd84863cbcd5653a8206d023fca45d
Vivean28 <vivean.la@gmail.com>
2021-06-24 21:40:54 -0700

    endpoints complete

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -1,2 +1,27 @@
 class CatsController < ApplicationController
+    def index
+        cats = Cat.all
+        render json: cats
+    endls
+
+      def create
+        # Create a new cat
+        cat = Cat.create(cat_params)
+
+        # respond with our new cat
+        render json: cat
+            end
+
+        # Handle strong parameters, so we are secure
+       
+
+            def update
+            end
+
+            def destroy
+            end
+     private
+        def cat_params
+            params.require(:cat).permit(:name, :age, :enjoys)
+        end
 end
